1cf34fa69749d1ac2e4d75d850aeb53c9c627795
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2023-08-21 20:08:10 -0300

    commit inicial

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Sobre mim</h2>
+        </div>
+    );
+}
+    <div></div>
+    
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Gabriel Jonatas</h1>
+            <div className="links">
+                <a href="/">Sobre mim</a>
+                <a href="/exp-ac">Experiências acadêmicas</a>
+                <a href="/exp-pr">Experiências profissionais</a>
+                <a href="/ex-hv">Habilidades e voluntariado</a>
+                <a href="/prj">Projetos</a>
+                <a href="/ativ">Atividades na entidade acadêmica GTHC</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
